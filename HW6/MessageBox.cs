2bd7546cdc78d9527093c95e2bd75e6065e7317b
MikKut <misha.kutny1@gmail.com>
2022-12-12 14:26:30 +0200

    Done

---
+++ HW6/MessageBox.cs
@@ -0,0 +1,32 @@
+﻿namespace HW6
+{
+    internal class MessageBox
+    {
+        public MessageBox()
+        {
+            WindowIsClosed = WtiteInConcoleResult;
+        }
+        public event Action<State> WindowIsClosed;
+        public async Task Open()
+        {
+            Console.WriteLine("Opened");
+            await Task.Delay(3000);
+            Console.WriteLine("Closed");
+            WindowIsClosed?.Invoke((State)(new Random().Next(0,2)));
+        }
+        private void WtiteInConcoleResult(State state)
+        {
+            switch(state)
+            {
+                case State.Cancel:
+                    Console.WriteLine("Oперацiя була вiдхилена");
+                    break;
+                case State.Ok:
+                    Console.WriteLine("Oперацiя була пiдтверджена");
+                    break;
+                default:
+                    throw new NotImplementedException("There is no such state: " + state);
+            }
+        }
+    }
+}
